1dd402e23398f673a3d347cbf9a091be77be3af6
Thierry Valet <tvalet@mphysx.com>
2025-09-08 08:39:39 +0200

    finalize supercell function

--- metasurf.py
+++ metasurf.py
@@ -1,12 +1,20 @@
+# Author: Thierry Valet
+# Copyright (C) 2025 - All rights reserved
+
+"""
+This script creates a chequerboard metasurface unit cell and a supercell by replicating the unit cell in x and y directions.
+"""
+# Import necessary modules
 import gmsh, sys
 
-
+# Function to create a unit cell with rectangles and circular cutouts
 def unit_cell(
         x0, # Bottom left corner x-coordinate
         y0, # Bottom left corner y-coordinate
         wx, # Width of the rectangles
         wy, # Height of the rectangles
         r,  # Radius of the circular region
+        h   # Mesh size at points
     ):
     # Creates lower left rectangle
     rect0 = gmsh.model.occ.addRectangle(x0, y0, 0, wx, wy)
@@ -90,63 +98,76 @@ def unit_cell(
 
     return [rect0_cut, rect1_cut, rect2_cut, rect3_cut], [dimTag0, dimTag1, dimTag2, dimTag3, dimTag4, dimTag5, dimTag6, dimTag7, dimTag8]
 
+# Function to create a supercell by replicating the unit cell
+def supercell(
+        nx, # Number of unit cells in x-direction
+        ny, # Number of unit cells in y-direction
+        wx, # Width of the unit cell
+        wy, # Height of the unit cell
+        r,  # Radius of the disks
+        h   # Mesh size at points
+        ):
+    list_of_rectangles0 = []
+    list_of_rectangles1 = []
+    list_of_disks = []
+    for i in range(nx):
+        for j in range(ny):
+            x0 = (2*i - nx)*wx
+            y0 = (2*j-ny)*wy
+            rectangles, disks = unit_cell(x0, y0, wx, wy, r, h)
+            list_of_rectangles0 += [rectangles[0][0][1]]
+            list_of_rectangles1 += [rectangles[1][0][1]]
+            list_of_rectangles1 += [rectangles[2][0][1]]
+            list_of_rectangles0 += [rectangles[3][0][1]]
+            list_of_disks += [disks[0][0][1]]
+            list_of_disks += [disks[1][0][1]]
+            list_of_disks += [disks[1][1][1]]
+            list_of_disks += [disks[2][0][1]]
+            list_of_disks += [disks[3][0][1]]
+            list_of_disks += [disks[3][1][1]]
+            list_of_disks += [disks[4][0][1]]
+            list_of_disks += [disks[4][1][1]]
+            list_of_disks += [disks[4][2][1]]
+            list_of_disks += [disks[4][3][1]]
+            list_of_disks += [disks[5][0][1]]
+            list_of_disks += [disks[5][1][1]]
+            list_of_disks += [disks[6][0][1]]
+            list_of_disks += [disks[7][0][1]]
+            list_of_disks += [disks[7][1][1]]
+            list_of_disks += [disks[8][0][1]]
+    return list_of_rectangles0, list_of_rectangles1, list_of_disks
 
+# Configuration parameters
 model_name = 'meta_surf'
-substrate_thickness = 0.01
+num_x = 5          # replication count in x
+num_y = 5          # replication count in y
 rect_x = 0.003      # rectangle dimension along x
 rect_y = 0.003      # rectangle dimension along y
 circle_r = 0.0005   # disk radius
-num_x = 5          # replication count in x
-num_y = 5          # replication count in y
 mesh_h = 1e-4
+substrate_thickness = 0.01
 
-
+# Initialize Gmsh and create the model
 gmsh.initialize()
-
 gmsh.model.add(model_name)
 
-list_of_rectangles_b = []
-list_of_rectangles_w = []
-list_of_disks = []
-for i in range(num_x):
-    for j in range(num_y):
-        x0 = (2*i - num_x)*rect_x
-        y0 = (2*j-num_y)*rect_y
-        rectangles, disks = unit_cell(x0, y0, rect_x, rect_y, circle_r)
-        list_of_rectangles_b += [rectangles[0][0][1]]
-        list_of_rectangles_w += [rectangles[1][0][1]]
-        list_of_rectangles_w += [rectangles[2][0][1]]
-        list_of_rectangles_b += [rectangles[3][0][1]]
-        list_of_disks += [disks[0][0][1]]
-        list_of_disks += [disks[1][0][1]]
-        list_of_disks += [disks[1][1][1]]
-        list_of_disks += [disks[2][0][1]]
-        list_of_disks += [disks[3][0][1]]
-        list_of_disks += [disks[3][1][1]]
-        list_of_disks += [disks[4][0][1]]
-        list_of_disks += [disks[4][1][1]]
-        list_of_disks += [disks[4][2][1]]
-        list_of_disks += [disks[4][3][1]]
-        list_of_disks += [disks[5][0][1]]
-        list_of_disks += [disks[5][1][1]]
-        list_of_disks += [disks[6][0][1]]
-        list_of_disks += [disks[7][0][1]]
-        list_of_disks += [disks[7][1][1]]
-        list_of_disks += [disks[8][0][1]]
-        # You can store or process the returned rectangles and disks as needed
+# Generate the supercell
+list_of_rectangles_b, list_of_rectangles_w, list_of_disks = supercell(num_x, num_y, rect_x, rect_y, circle_r, mesh_h)
 
+# Remove duplicates
 gmsh.model.occ.removeAllDuplicates()
 
+# Synchronize the CAD kernel with the Gmsh model
 gmsh.model.occ.synchronize()
 
+# Define physical groups for the metasurface components
 gmsh.model.addPhysicalGroup(2, list_of_rectangles_b, tag=1, name="rectangles_b")
 gmsh.model.addPhysicalGroup(2, list_of_rectangles_w, tag=2, name="rectangles_w")
 gmsh.model.addPhysicalGroup(2, list_of_disks, tag=3, name="disks")
 
+# Display the generated metasurface
+gmsh.fltk.run()
 
 # gmsh.model.mesh.generate()
-
-# Display the unit cell
-gmsh.fltk.run()
 
 gmsh.finalize()
